ac7b531a5c90aa14cb1cf4461c38ddec22dc8e38
Thales <thales.flores@icommgroup.com.br>
2018-04-18 16:04:58 -0300

    fallback

---
+++ lib/collava_api_web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule CollavaApiWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use CollavaApiWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(CollavaApiWeb.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(CollavaApiWeb.ErrorView, :"404")
+  end
+end
